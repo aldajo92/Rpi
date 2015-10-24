34a91ab9a91042875f919069d79e485ab1e0fa98
aldajo92 <alejodiscovery20@gmail.com>
2015-10-24 15:20:57 -0500

    add for to blink

--- blink.py
+++ blink.py
@@ -1,5 +1,10 @@
 # http://www.thirdeyevis.com/pi-page-2.php
 import RPi.GPIO as GPIO ## Import GPIO library
+import time ## Import Time library
 GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
 GPIO.setup(7, GPIO.OUT) ## Setup GPIO Pin 7 to OUT
-GPIO.output(7,True) ## Turn on GPIO pin 7
+for i in range(0,10):
+  GPIO.output(7,True) ## Turn on GPIO pin 7
+  time.sleep(1)
+  GPIO.output(7,True) ## Turn off GPIO pin 7
+  time.sleep(1)
